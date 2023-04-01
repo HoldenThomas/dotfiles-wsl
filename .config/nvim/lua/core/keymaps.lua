0586b030fa016cb3b5c16902ed38a0be18e9a9da
holden <spiritupload@protonmail.com>
2023-04-01 06:40:45 -0400

    add nvim lua, starship, tmux configs

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.cmd [[ set clipboard+=unnamedplus ]]
+vim.cmd [[ set number relativenumber ]]
+vim.cmd [[ set mouse=a ]]
+vim.cmd [[ set ignorecase ]]
+vim.cmd [[ set smartcase ]]
+vim.cmd [[ set splitbelow splitright ]]
+
+-- Indenting visual block doesn't go out of visual block
+vim.cmd [[ vmap < <gv ]]
+vim.cmd [[ vmap > >gv ]]
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
